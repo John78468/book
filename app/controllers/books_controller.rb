fe7de7b54181aef265640afe3d50d19f07240f7d
cardon <philantrope1@outlook.com>
2024-01-23 15:58:04 +0100

    new and formulaire

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,4 +6,12 @@ class BooksController < ApplicationController
   def index
     @books = Book.all
   end
+
+  def new
+    @book = Book.new
+  end
+
+  def create
+    @book = Book.create
+  end
 end
