ae147bf14c99443fba4ac170edbda9df6e8209b0
cardon <philantrope1@outlook.com>
2024-01-30 11:02:19 +0100

    update

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -12,8 +12,28 @@ class BooksController < ApplicationController
   end
 
   def create
-    @book = Book.create(book_params)
+  @book = Book.new(book_params)
+
+  if @book.save
     redirect_to books_path
+  else
+    render :new
+  end
+
+end
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    @book = Book.find(params[:id])
+
+    if @book.update(book_params)
+      redirect_to books_path
+    else
+      render :new
+    end
   end
 
   private
